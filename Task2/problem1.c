a05dfbcd9368df707ae5be1a8bbf8061a043d91f
Yasmine <shroomz.ftw@gmail.com>
2015-02-15 09:32:56 +0000

    Completed Problem 1

--- Task2/problem1.c
+++ Task2/problem1.c
@@ -1,27 +1,71 @@
-#include <stdio.h>      /* puts, printf */
-#include <time.h>       /* time_t, struct tm, time, localtime */
+#include <stdio.h> /* puts, printf */
+#include <time.h> /* time_t, struct tm, time, localtime */
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
-int main ()
-{
-  time_t rawtime;
-  struct tm * timeinfo;
-  FILE * fp;
-  FILE * fr;
+/*before this was extended into task 1.2 everything was in the main..split
+them into methods cus it looked/worked cleaner */ 
+
+time_t rawtime;
+struct tm * timeinfo;
+FILE * fp;
+FILE * fr;
+
+#define DAEMON_NAME "vdaemon"
+
+void logTime() {
+/*based on task 1.1..*/
 while ( 1 ) {
-  time (&rawtime);
-  timeinfo = localtime (&rawtime);
-  fr=fopen("/tmp/test.txt","a");
-  if(fr==NULL) {
-  fp=fopen("/tmp/test.txt","w+");
-  fprintf(fp,"Current local time and date: %s", asctime(timeinfo));
-  fclose(fp);
- }
- else {
-  fprintf(fr,"Current local time and date: %s", asctime(timeinfo));
-  fclose(fr);
- }
- sleep(5);
+time (&rawtime);
+timeinfo = localtime (&rawtime);
+fr=fopen("/tmp/test.txt","a");
+if(fr==NULL) {
+fp=fopen("/tmp/test.txt","w+");
+fprintf(fp,"Current local time and date: %s", asctime(timeinfo));
+fclose(fp);
+			}
+else {
+fprintf(fr,"Current local time and date: %s", asctime(timeinfo));
+fclose(fr);
+		}
+sleep(5);     //Sleep for 5 seconds
+
+	}
 }
 
-  return 0;
+
+int main () {
+    /*----------------*/
+    /*--Daemon stuff--*/
+    /*----------------*/
+
+     pid_t process_id, sid;
+     process_id=fork();
+
+ // FORK FAILED.
+	if (process_id < 0) { 
+		printf("fork failed!\n");
+	// Return failure in exit status
+	exit(EXIT_FAILURE);
+	}
+
+// KILLING PARENT PROCESS.
+	if (process_id > 0) {
+	printf("process_id of child process %d \n", process_id);
+	// return success in exit status
+	exit(EXIT_SUCCESS);
+ }
+
+//set new session
+sid = setsid();
+if(sid < 0) { exit(EXIT_FAILURE); }
+
+    chdir("/tmp");
+
+    /*--Main Program--*/
+    while(1){ logTime(); }//Run our Process
+            
 }
