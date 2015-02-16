13ef3a4e5bcf3a6fcd1ccf84bd01ae1339a32b46
Yasmine <shroomz.ftw@gmail.com>
2015-02-16 06:09:21 +0000

    Update problem2.c

--- Task2/problem2.c
+++ Task2/problem2.c
@@ -1,6 +1,11 @@
-#include <stdio.h> /* puts, printf */ 
-
-int main(int argc, char** argv[]) {
-  execve("bin/ps",argv[0])
-  return(0);
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    int main(int argc, char *argv[])
+    {
+    char *newargv[] = { "/bin/ps", "-l", NULL };
+    char *newenviron[] = { NULL };
+    char *exeprog = "/bin/ps";
+    execve(exeprog, newargv, newenviron);
+    exit(EXIT_FAILURE);
 }
