510fd540c6c3084565a29370755886f0e31cc427
Yasmine <shroomz.ftw@gmail.com>
2015-03-02 06:00:57 +0000

    Update Task31.c

--- Task3/Task31.c
+++ Task3/Task31.c
@@ -32,18 +32,20 @@ pid_t process_ids[N];
 /*
 * Make N forks, call dummy in every child.
 */
-for(int i=0;i<N;i++){
+int i;
 
-	process_ids[i] = fork()
+for(i=0;i<N;i++){
+
+	process_ids[i] = fork();
 
 	if(process_ids[i]<0)	{
 	printf("fork failed\n");
-	exit(EXIT_FAILURE);
+	exit(0);
 		}	
 	//child process
 	else if (process_ids[i]==0)	{ 
 		dummy(0);
-		exit(EXIT_SUCESS);	
+		exit(0);	
 	}
 
 }
@@ -54,23 +56,24 @@ clock_gettime (CLOCK_REALTIME, &stop);
  *TODO: Wait for the forks.	
  *The wait system-call puts the process to sleep and waits for a child-process to end. 
 */
+int j;
 
-for(j= 0;j< N;j++)   {         wait(&pids[j]);        }
+for(j= 0;j< N;j++)   {         wait(&process_ids[j]);        }
 
 clock_gettime (CLOCK_REALTIME, &finish);
 
 
-printf ("%d proc: fork=%d (mics) wait=%d (mics) sum=%d (mics)\n",
+printf ("%d proc: fork=%5.0f (mics) wait=%5.0f (mics) sum=%5.0f (mics)\n",
 N, xelapsed (stop, start),
-xelapsed (finish, stop), xelapsed (finish, start);
-printf ("per proc: fork=%d (mics) wait=%d (mics) sum=%d (mics)\n",
+xelapsed (finish, stop), xelapsed (finish, start));
+printf ("per proc: fork=%5.0f (mics) wait=%5.0f (mics) sum=%5.0f (mics)\n",
 xelapsed (stop, start)/N,
-xelapsed (finish, stop)/N, xelapsed (finish, start)/N;
+xelapsed (finish, stop)/N, xelapsed (finish, start)/N);
 
 }
 
 
-int main(int argc, char*argv) {
+int main(int argc, char*argv[]) {
 
 int n;
 
